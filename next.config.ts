17da668d1dc4b3eb1819922a617e1f703bc70748
Aidan Tan <kayjien@gmail.com>
2025-08-13 03:17:03 +0800

    Moved images directory

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  output: 'export', 
+    images: {
+    unoptimized: true, // This disables the server-side image optimizer
+  },
 };
 
+module.exports = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/aidantanwebportfolio' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/aidantanwebportfolio' : '',
+}
 export default nextConfig;
