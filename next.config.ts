fb549c43d09499c2328af3cf64d22952c6560751
Aidan Tan <kayjien@gmail.com>
2025-08-14 06:10:56 +0800

    Fixed basePath src reference

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+export const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/aidantanwebportfolio',
 };
 
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = { basePath: isProd ? '/aidantanwebportfolio' : '', images: { unoptimized: true, }, };
-  
-// module.exports = nextConfig;
+module.exports = nextConfig;
