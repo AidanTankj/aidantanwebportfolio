e60112b5e8fc64398e32e5b0262f618acb34a9f8
Aidan Tan <kayjien@gmail.com>
2025-08-31 00:25:36 +0800

    Added config to accept image hostnames

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,24 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/aidantanwebportfolio',
+
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.rxresu.me',
+        pathname: '**', // This is a wildcard that allows all paths from this hostname
+      },
+
+  {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        pathname: '**', // This is a wildcard that allows all paths from this hostname
+      },
+    ],
+  },
+
 };
 
 export default nextConfig;
